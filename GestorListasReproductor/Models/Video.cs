0063ddc577bbe18e3d0d6840750576d5227fd761
Franco Sosa <sosa.franco@transcore.com>
2023-07-19 01:31:15 -0300

    Agregar archivos de proyecto.

---
+++ GestorListasReproductor/Models/Video.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorListasReproductor.Models
+{
+    public class Video
+    {
+        public int Duracion { get; set; }
+        public string Autor { get;set; }
+        public string Titulo{ get; set; }
+        public string CuandoSePublico { get; set; }
+    }
+}
